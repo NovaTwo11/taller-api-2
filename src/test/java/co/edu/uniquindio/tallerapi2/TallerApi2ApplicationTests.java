72b3c68d89745fb760514fff5e1b29bb3c1bb145
HoldanLopez <holdann.lopezs@uqvirtual.edu.co>
2025-11-01 20:26:12 -0500

    Reto 4 automatizacion v.2.6

--- src/test/java/co/edu/uniquindio/tallerapi2/TallerApi2ApplicationTests.java
+++ src/test/java/co/edu/uniquindio/tallerapi2/TallerApi2ApplicationTests.java
@@ -2,8 +2,10 @@ package co.edu.uniquindio.tallerapi2;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class TallerApi2ApplicationTests {
 
     @Test
